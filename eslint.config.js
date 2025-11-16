47b29ae64e3f51e48290b3f7e3b382ed1eee915b
Moshe Ben Peshe <6439152+baconpantsuppercut@users.noreply.github.com>
2025-11-15 22:19:14 -0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,9 @@
 import js from "@eslint/js";
-import tseslint from "typescript-eslint";
 
 export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
   {
-    ignores: ["dist/**"],
-
-    rules: {
-      // safe minimal ruleset for early project phase
-      "no-unused-vars": "warn",
-      "no-undef": "off"
-    }
+    ...js.configs.recommended,
+    files: ["client/**/*.js", "scripts/**/*.js"],
+    ignores: ["dist/**", "node_modules/**"]
   }
 ];
